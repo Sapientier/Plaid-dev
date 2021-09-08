44d77fe795540c81ea5bce48429511b7390c61df
Brian Torres <luxormagnes@gmail.com>
2021-09-08 12:24:32 -0400

    Push inicial

---
+++ client/src/App.js
@@ -0,0 +1,70 @@
+import React, { useCallback, useState, useEffect } from "react";
+import { usePlaidLink } from "react-plaid-link";
+import './App.css';
+import logo from './logo.svg';
+
+const PlaidLink = ({ token }) => {
+    const onSuccess = useCallback(
+        (token) => {
+            // send public_token to server
+            const response = fetch('/set_access_token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: `public_token=${JSON.stringify({ token })}`,
+            });
+            // Handle response ...
+            return response;
+        },
+        []
+    );
+
+    const config = {
+        token,
+        onSuccess,
+        // onExit
+        // onEvent
+    };
+
+    const { open, ready } = usePlaidLink(config);
+
+    return (
+        <button className="button" onClick={() => open()} disabled={!ready}>
+            Connect a bank account
+        </button>
+    );
+};
+
+const App = () => {
+    const [token, setToken] = useState(null);
+
+    // generate a link_token
+    useEffect(() => {
+        async function createLinkToken() {
+            let response = await fetch("/create_link_token", {
+                method: "POST",
+            });
+            const { link_token } = await response.json();
+            setToken(link_token);
+        }
+        createLinkToken();
+    }, []);
+
+    // only initialize Link once our token exists
+    return (
+        <div className="centerDiv">
+            {token === null ? (
+                // insert your loading animation here
+                <div className="loader"></div>
+            ) : (
+                <>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <PlaidLink token={token} />
+                </>
+            )}
+        </div>
+    )
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,127 @@
+// Using Express
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const keys = require('./config/keys');
+const log = console.log;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded())
+app.use(cors({ origin: 'http://localhost:5000' }));
+app.set('view engine', 'ejs');
+
+const plaid = require('plaid');
+
+const client = new plaid.Client({
+    clientID: keys.CLIENT_ID,
+    secret: keys.PLAID_SECRET,
+    env: plaid.environments.sandbox,
+});
+
+let accessT;
+
+console.log(client)
+
+app.get('/', (req, res) => {
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+    res.render('pages/index');
+})
+
+app.post('/create_link_token', async (req, res) => {
+    try {
+        const tokenResponse = await client.createLinkToken({
+            user: {
+                client_user_id: 'test_dev',
+            },
+            client_name: 'Roger Manzo',
+            products: ["transactions", "auth"],
+            country_codes: ['US'],
+            language: 'es',
+            webhook: 'https://postb.in/1630615487525-6100976159796',
+        });
+        res.json(tokenResponse);
+    } catch (e) {
+        // Display error on client
+        return res.send({ error: e.message });
+    }
+});
+
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+// https://plaid.com/docs/#exchange-token-flow
+app.post('/set_access_token', async (request, response) => {
+    PUBLIC_TOKEN = request.body.public_token;
+    try {
+        const tokenResponse = await client.itemPublicTokenExchange({
+            public_token: PUBLIC_TOKEN,
+        });
+        prettyPrintResponse(tokenResponse);
+        ACCESS_TOKEN = tokenResponse.data.access_token;
+        ITEM_ID = tokenResponse.data.item_id;
+        response.json({
+            access_token: ACCESS_TOKEN,
+            item_id: ITEM_ID,
+            error: null,
+        });
+    } catch (error) {
+        prettyPrintResponse(error.response);
+        return response.json(formatError(error.response));
+    }
+});
+
+app.post('/exchange_public_token', async (req, res) => {
+    try {
+        const exchangeTokenResponse = await client.exchangePublicToken(
+            req.body.public_token
+        );
+        const accessToken = exchangeTokenResponse.access_token;
+        accessT = accessToken;
+        res.redirect(307, '/item/fire_webhooks/');
+    } catch (e) {
+        res.json({ error: e });
+    }
+});
+
+app.post('/item/fire_webhooks', async (req, res) => {
+    const response = await client.sandboxItemFireWebhook(accessT, 'DEFAULT_UPDATE').catch((err) => {
+        res.status(402)
+            .json({
+                status: 'error',
+                message: err
+            });
+    });
+    res.status(202)
+        .json({
+            status: 'approved',
+            data: response
+        });
+    /* res.redirect('/transactions/get'); */
+});
+
+
+app.get('/transactions/get', async (req, res) => {
+
+    const response = await client
+        .getTransactions(accessT, '2020-01-01', '2020-02-01', {})
+        .catch((err) => {
+            log({ error: err.message });
+        });
+    const transactions = response.transactions
+    const totalTransactions = response.total_transactions;
+    res.status(202)
+        .json({
+            status: 'approved',
+            data: transactions,
+            total_transactions: totalTransactions
+        });
+    log(response);
+});
+
+
+
+app.listen(5000, () => log('server on port 5000'));
